289b6390ade6378b85f9f2de20f8f719e131c26e
kingzcheung <kingzcheung@gmail.com>
2026-04-06 17:45:51 +0800

    release 1.1.0
    Signed-off-by: kingzcheung <kingzcheung@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,7 +34,7 @@ android {
         minSdk = 28
         targetSdk = 35
         versionCode = 1
-        versionName = "1.0.1"
+        versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         
