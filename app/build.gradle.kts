d70f83f895453eca27eb5725760d95b01d1792a4
Kor1 <kingzcheung@gmail.com>
2026-04-08 22:18:40 +0800

    release: bump version to 1.2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,8 +34,8 @@ android {
         applicationId = "com.kingzcheung.kime"
         minSdk = 28
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.1.0"
+        versionCode = 2
+        versionName = "1.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         
