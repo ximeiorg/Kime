012f6acadaf95f898776dd64e69dcafb6cc14ddd
kingzcheung <kingzcheung@gmail.com>
2026-04-09 17:04:58 +0800

    chore: bump version to v1.2.1 and enforce v-prefixed tags

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,8 +34,8 @@ android {
         applicationId = "com.kingzcheung.kime"
         minSdk = 28
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.2.0"
+        versionCode = 3
+        versionName = "1.2.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         
