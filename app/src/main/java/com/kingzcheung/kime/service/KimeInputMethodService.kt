7a496ea3cbb16a40b2e185b913b022f5747be01b
Kor1 <kingzcheung@gmail.com>
2026-04-28 23:07:23 +0800

    wip

--- app/src/main/java/com/kingzcheung/kime/service/KimeInputMethodService.kt
+++ app/src/main/java/com/kingzcheung/kime/service/KimeInputMethodService.kt
@@ -16,6 +16,7 @@ import android.widget.Toast
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.CompositionLocalProvider
